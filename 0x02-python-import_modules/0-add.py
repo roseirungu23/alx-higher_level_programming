de6a27ad60954910a0f23b4d023eb167495683b3
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-11 03:47:50 -0700

    0. Import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-from add_0.py import add
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
