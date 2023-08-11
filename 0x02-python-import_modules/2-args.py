f94ea6f16ff1886b1258d62f4ce4cb6342bdf52e
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-11 06:20:24 -0700

    2. How to make arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     number = len(sys.argv) - 1
     if number == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif number == 1:
         print("1 argument:")
     else:
