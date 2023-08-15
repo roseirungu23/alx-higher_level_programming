9d88f15e5ee4c3b130fa8ea3d07e32491e6e952a
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-15 11:58:01 -0700

    remove a letter

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     for char in my_string:
-        my_string.remove(char(c) and char (C))
+        if char == 'c' and char == 'C':
+            my_string.remove()
         return my_string
