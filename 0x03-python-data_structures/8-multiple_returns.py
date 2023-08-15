b0d7529c5ff8037558b3a3d8a5631425a0fcce13
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-15 12:56:11 -0700

    more returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        char[0] = None
+    else:
+        char[0] = sentence[0]
+
+    return (len(sentence), char[0])
