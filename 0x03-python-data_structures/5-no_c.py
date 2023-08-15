4cf839f6b6d084303da180532c54f1f8c841fc04
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-15 12:01:32 -0700

    remove char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     for char in my_string:
         if char == 'c' and char == 'C':
-            my_string.remove()
-        return my_string
+            new_string += char
+            return new_string
