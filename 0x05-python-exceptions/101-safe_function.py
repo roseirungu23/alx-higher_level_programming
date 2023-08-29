7241ac4846f790efbed8a698b58b1b31d527adb5
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-29 04:53:46 -0700

    Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -14,6 +14,7 @@ def safe_function(fct, *args):
 def my_div(a, b):
     return a / b
 
+
 result = safe_function(my_div, 10, 2)
 print("result of my_div: {}".format(result))
 
@@ -27,6 +28,7 @@ def print_list(my_list, len):
         print(my_list[i])
         i += 1
     return len
+
 
 result = safe_function(print_list, [1, 2, 3, 4], 10)
 print("result of print_list: {}".format(result))
