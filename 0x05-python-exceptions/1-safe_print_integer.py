c5d844cf8d19b7c1bc3abd0ea26b81ac90571cd2
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-29 02:36:07 -0700

    Safe printing of an integers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -7,7 +7,7 @@ def safe_print_integer(value):
     except (ValueError, TypeError):
         return (False)
 
-value = 89
+    value = 89
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
