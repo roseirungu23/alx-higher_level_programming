7267718a0ca92c8f87c5d4ab37a433edc26f92b6
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-29 04:01:15 -0700

    Raise a message

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -3,7 +3,7 @@ def raise_exception_msg(message=""):
     try:
         raise NameError(message)
     except NameError as error:
-        print(f"NameError: {error}")
+        print(f"{error}")
 
 
 raise_exception_msg("Python is awesome.")
