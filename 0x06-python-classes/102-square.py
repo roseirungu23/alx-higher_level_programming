30c43687fbe1100e3bac473fc25a1016cd63d1bb
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-30 12:02:04 -0700

    Compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class square."""
+
+
+class Square:
+    """Represent the square"""
+    def __init__(self, size=0):
+        """initialize the size of the new square.
+         Args:
+            size (int): The size of the new square."""
+            self.size = size
+
