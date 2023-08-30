c9cee6b3924aaec5a9269911c919cd0f1159e7b0
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-30 03:25:34 -0700

    private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """Represents new square"""
+    def __init(self, size=0):
+        """initializes new square
+
+        Args:
+        size(int): the size of new square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
