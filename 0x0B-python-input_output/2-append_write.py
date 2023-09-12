bae07f4b1cf2f85c9d64d82c9ef6964d93889029
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-09-12 10:48:12 -0700

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bbin/python3
+"""
+Defines function append_write
+"""
+
+
+def append_write(filename="", text=""):
+    """ appends a string at the end of a text file (UTF8) 
+    returns the number of characters added"""
+    with open(filename, "a", encoding="UTF-8") as file:
+        return file.write(text)
