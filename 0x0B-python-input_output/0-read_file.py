f35ea4b86bca92b031dcc2946a3cb9b9122f65cc
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-09-12 10:17:14 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Defines a function read_file
+"""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
