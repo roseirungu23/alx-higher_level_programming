3e8cb69f6d1600a2d4bc5343f22969a50f33ee31
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-10-17 07:46:39 -0700

    SQL

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- That converts hbtn_0c_0 database
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
